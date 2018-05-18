4e5b3f8c76516116dfe436d3bed7b5c17d74442b
Errietta Kostala <errietta@errietta.me>
2018-05-18 02:47:29 +0100

    uploadin' statements

--- src/components/Report/ReportComponent.tsx
+++ src/components/Report/ReportComponent.tsx
@@ -1,20 +1,66 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import moment from 'moment';
+
+import { Report, ReportFactory, Category, Categoriser } from 'hyperbudget-core';
+import { HTMLFileManager } from '../../lib/manager/htmlfilemanager';
+import { StatementUploaderComponent } from '../StatementUploader/StatementUploaderComponent';
 
 interface ReportRouteComponentProps {
   month: string,
 };
 
-export class ReportComponent extends React.Component<RouteComponentProps<ReportRouteComponentProps>, {}> {
-  componentDidMount() {
+interface ReportComponentState {
+  report: Report,
+}
+
+export class ReportComponent extends React.Component<RouteComponentProps<ReportRouteComponentProps>, ReportComponentState> {
+  month: string;
+  state = {
+    report: null,
+  };
+
+  componentDidMount(): void {
     console.log(this.props);
     let month: string = this.props.match.params.month;
     console.log(month);
+    this.month = month || moment().format('YYYYMM');
   }
+
+  private onFileSelected = (file: File, type: string): void => {
+    console.log(file);
+
+    HTMLFileManager.read_file(file).then((txt: string) => {
+      console.log(txt);
+      this.loadStatement(txt, type);
+    });
+  };
+  
+  private loadStatement = (csv_text: string, type: string): void => {
+    const rf: ReportFactory = new ReportFactory();
+    const categories: Category[] = [];
+    const categoriser: Categoriser = new Categoriser(categories);
+
+    rf.from_csv(csv_text, type)
+      .then(() => categoriser.categorise_transactions(rf.report.transactions))
+      .then(() => {
+        const report: Report = rf.report;
+        report.filter_month(this.month);
+        console.log(report.transactions);
+        
+        this.setState({
+          report: report,
+        });
+      });
+  };
 
   render() {
     return (
-      <h1>Report!</h1>
+      <div className='Report'>
+        <h1>Report!</h1>
+        <StatementUploaderComponent onFileSelected={ this.onFileSelected } />
+        { this.state.report ? <div>we have a report</div> : <div>none</div> }
+      </div>
     );
   }
 }
