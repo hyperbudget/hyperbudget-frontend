c4a4582d4c45f7c2609513c868b685dc00ebfaad
Errietta Kostala <errietta@errietta.me>
2018-07-24 18:02:35 +0100

    start on tests

---
+++ src/components/Register/RegisterComponent.spec.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { RegisterComponent } from './RegisterComponent';
+import renderer from 'react-test-renderer';
+import * as enzyme from 'enzyme';
+
+import Adapter from 'enzyme-adapter-react-16';
+
+import 'jest';
+
+beforeAll(() => {
+  enzyme.configure({ adapter: new Adapter() });
+});
+
+test('Register component renders', () => {
+  let component = renderer.create(
+    <RegisterComponent
+      doRegister={() => {}}
+      isLoggedIn={false}
+      token={null}
+      APIErrors={null}
+    />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Button calls doRegister', () => {
+  const mockRegister = jest.fn();
+
+  let component = enzyme.shallow(
+    <RegisterComponent
+      doRegister={mockRegister}
+      isLoggedIn={false}
+      token={null}
+      APIErrors={null}
+    />
+  );
+
+  component.find('#registerButton').simulate('click');
+
+  expect(mockRegister.mock.calls.length).toBe(1);
+  expect(mockRegister.mock.calls[0]).toMatchSnapshot();
+});
+
