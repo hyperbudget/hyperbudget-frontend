cb2b924a9a3d66f581118fb4453edab1215fcfca
Errietta Kostala <errietta@errietta.me>
2018-07-28 14:59:27 +0100

    Add version route

---
+++ src/components/VersionComponent/VersionComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VersionComponent = (props) => (
+  <div className='main' style={{marginTop: '40px'}}><h1>Version: {`[AIV]{version}[/AIV]`}</h1></div>
+);
+
+export default VersionComponent;
