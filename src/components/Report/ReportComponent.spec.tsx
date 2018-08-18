59ff6130894720d72507314873b1749bd90652aa
Robert Main <robert.main3@gmail.com>
2018-08-18 18:31:11 -0400

    Promise Usage
    Usage of async/await makes the codebase a little cleaner than returning a
    new promise and calling resolve() several times

--- src/components/Report/ReportComponent.spec.tsx
+++ src/components/Report/ReportComponent.spec.tsx
@@ -68,148 +68,141 @@ describe('Report', () => {
     expect(component.html()).toMatchSnapshot();
   });
 
-  it('component renders for transactions', () => {
-    return new Promise((resolve, reject) => {
-      const transactions = [
-        {
-          txn_amount_credit: 0,
-          txn_amount_debit: 100,
-          txn_desc: 'Hello World',
-          txn_date: '2017-12-31T00:00:00Z',
-          txn_src: 'hsbc',
-          txn_type: 'DEB',
-          acc_sortcode: '',
-          acc_balance: 0,
-          acc_number: '',
-        },
-        {
-          txn_amount_credit: 0,
-          txn_amount_debit: 150,
-          txn_desc: 'Nano software',
-          txn_date: '2018-01-01T00:00:00Z',
-          txn_src: 'hsbc',
-          acc_sortcode: '',
-          acc_balance: 0,
-          acc_number: '',
-          txn_type: 'DEB',
-        },
-        {
-          txn_amount_credit: 0,
-          txn_amount_debit: 150,
-          txn_desc: 'Electricity',
-          txn_date: '2018-01-01T00:00:00Z',
-          txn_src: 'hsbc',
-          acc_sortcode: '',
-          acc_balance: 0,
-          acc_number: '',
-          txn_type: 'DD',
-        },
-        {
-          txn_amount_credit: 0,
-          txn_amount_debit: 50,
-          txn_desc: 'Water',
-          txn_date: '2017-12-01T00:00:00Z',
-          txn_src: 'hsbc',
-          acc_sortcode: '',
-          acc_balance: 0,
-          acc_number: '',
-          txn_type: 'DD',
-        },
-        {
-          txn_amount_credit: 0,
-          txn_amount_debit: 200,
-          txn_desc: 'Uplink Fee',
-          txn_date: '2018-02-01T00:00:00Z',
-          txn_src: 'hsbc',
-          acc_sortcode: '',
-          acc_balance: 0,
-          acc_number: '',
-          categories: [],
-        },
-      ];
+  it('component renders for transactions', async () => {
+    const transactions = [
+      {
+        txn_amount_credit: 0,
+        txn_amount_debit: 100,
+        txn_desc: 'Hello World',
+        txn_date: '2017-12-31T00:00:00Z',
+        txn_src: 'hsbc',
+        txn_type: 'DEB',
+        acc_sortcode: '',
+        acc_balance: 0,
+        acc_number: '',
+      },
+      {
+        txn_amount_credit: 0,
+        txn_amount_debit: 150,
+        txn_desc: 'Nano software',
+        txn_date: '2018-01-01T00:00:00Z',
+        txn_src: 'hsbc',
+        acc_sortcode: '',
+        acc_balance: 0,
+        acc_number: '',
+        txn_type: 'DEB',
+      },
+      {
+        txn_amount_credit: 0,
+        txn_amount_debit: 150,
+        txn_desc: 'Electricity',
+        txn_date: '2018-01-01T00:00:00Z',
+        txn_src: 'hsbc',
+        acc_sortcode: '',
+        acc_balance: 0,
+        acc_number: '',
+        txn_type: 'DD',
+      },
+      {
+        txn_amount_credit: 0,
+        txn_amount_debit: 50,
+        txn_desc: 'Water',
+        txn_date: '2017-12-01T00:00:00Z',
+        txn_src: 'hsbc',
+        acc_sortcode: '',
+        acc_balance: 0,
+        acc_number: '',
+        txn_type: 'DD',
+      },
+      {
+        txn_amount_credit: 0,
+        txn_amount_debit: 200,
+        txn_desc: 'Uplink Fee',
+        txn_date: '2018-02-01T00:00:00Z',
+        txn_src: 'hsbc',
+        acc_sortcode: '',
+        acc_balance: 0,
+        acc_number: '',
+        categories: [],
+      },
+    ];
 
-      const categories: Category[] = [
-        {
-          category_rules: {
-            txn_desc: {
-              "rules": [
-                ['=', 'Hello World']
-              ]
-            }
-          },
-          name: 'Hello',
-          id: 'hello',
-          className: '',
-          txn_month_modifier: 1,
+    const categories: Category[] = [
+      {
+        category_rules: {
+          txn_desc: {
+            "rules": [
+              ['=', 'Hello World']
+            ]
+          }
         },
-        {
-          category_rules: {
-            txn_type: {
-              rules: [
-                ['=', 'DD']
-              ]
-            }
-          },
-          name: 'Direct Debit',
-          id: 'directdebit',
-          className: '',
+        name: 'Hello',
+        id: 'hello',
+        className: '',
+        txn_month_modifier: 1,
+      },
+      {
+        category_rules: {
+          txn_type: {
+            rules: [
+              ['=', 'DD']
+            ]
+          }
         },
-      ];
+        name: 'Direct Debit',
+        id: 'directdebit',
+        className: '',
+      },
+    ];
 
-      const store = mockStore({
-        user: {
-          transactions: transactions,
-          categories: categories,
-          txnPassword: 'pass',
-          token: 'abc',
-          isLoggedIn: true,
-        },
-      });
-
-      mount(store,
-        <ReportComponent
-          date={moment('2018-01-01').utc().toDate()}
-          onUpdate={transactions => {
-            expect(transactions).toMatchSnapshot();
-            resolve();
-          }}
-        />);
-
-      mount(store,
-        <ReportComponent
-          date={moment('2018-02-01').utc().toDate()}
-          onUpdate={transactions => {
-            expect(transactions).toMatchSnapshot();
-            resolve();
-          }}
-        />);
-
-      mount(store,
-        <ReportComponent
-          date={moment('2017-12-01').utc().toDate()}
-          onUpdate={transactions => {
-            expect(transactions).toMatchSnapshot();
-            resolve();
-          }}
-        />);
-
-      mount(store,
-        <ReportComponent
-          date={moment('2017-11-01').utc().toDate()}
-          onUpdate={transactions => {
-            expect(transactions).toMatchSnapshot();
-            resolve();
-          }}
-        />);
-
-      mount(store,
-        <ReportComponent
-          date={moment('2018-06-01T00:00:00+00:00').toDate()}
-          onUpdate={transactions => {
-            expect(transactions).toMatchSnapshot();
-            resolve();
-          }}
-        />);
+    const store = mockStore({
+      user: {
+        transactions: transactions,
+        categories: categories,
+        txnPassword: 'pass',
+        token: 'abc',
+        isLoggedIn: true,
+      },
     });
+
+    await mount(store,
+      <ReportComponent
+        date={moment('2018-01-01').utc().toDate()}
+        onUpdate={transactions => {
+          expect(transactions).toMatchSnapshot();
+        }}
+      />);
+
+    await mount(store,
+      <ReportComponent
+        date={moment('2018-02-01').utc().toDate()}
+        onUpdate={transactions => {
+          expect(transactions).toMatchSnapshot();
+        }}
+      />);
+
+    await mount(store,
+      <ReportComponent
+        date={moment('2017-12-01').utc().toDate()}
+        onUpdate={transactions => {
+          expect(transactions).toMatchSnapshot();
+        }}
+      />);
+
+    await mount(store,
+      <ReportComponent
+        date={moment('2017-11-01').utc().toDate()}
+        onUpdate={transactions => {
+          expect(transactions).toMatchSnapshot();
+        }}
+      />);
+
+    await mount(store,
+      <ReportComponent
+        date={moment('2018-06-01T00:00:00+00:00').toDate()}
+        onUpdate={transactions => {
+          expect(transactions).toMatchSnapshot();
+        }}
+      />);
   });
 });
