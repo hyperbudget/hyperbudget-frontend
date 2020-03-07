8e972b61620a0a0064acf483a07dee6a0fb93034
Errietta Erry Kostala <errietta@errietta.me>
2020-03-07 17:22:18 +0000

    Revert "force matching multiple rules when applying custom categories"
    This reverts commit ee454c1a7a24a3077c7ce1b00234493ec3f37cba.

--- src/components/Report/ReportComponent.tsx
+++ src/components/Report/ReportComponent.tsx
@@ -309,8 +309,6 @@ class ReportComponent extends React.Component<ReportComponentProps, ReportCompon
       // accidentally, this time it's 100% on purpose. I love js.
       currentCat = this.removeCustomCategory(currentCat, txn);
 
-      currentCat.category_rules.mode = RuleMatchMode.Flex;
-
       if (categorisationType === CategorisationType.IDENTIFIER) {
         if (
           currentCat.category_rules.identifier &&
