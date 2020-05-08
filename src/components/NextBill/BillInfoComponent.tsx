9744b8f1293de7549d7b805825ecada17753fbf9
Errietta Erry Kostala <errietta@users.noreply.github.com>
2020-05-08 14:12:09 +0100

    Total pending (#49)
    * Update BillInfoComponent.tsx
    
    * Update package.json
    
    * estimated totals

--- src/components/NextBill/BillInfoComponent.tsx
+++ src/components/NextBill/BillInfoComponent.tsx
@@ -8,15 +8,25 @@ interface BillInfoComponentProps {
   type: string,
 }
 
-export const BillInfoComponent = (props: BillInfoComponentProps) => (
-  <div>
-    <p>Next {props.type}:</p>
-    <ul>
-    {
-      props.transactions.map((transaction, idx) => (
-        <li key={idx}>{transaction.description}, taken around the {moment(transaction.date).format('Do')}, for about <b>&pound;{transaction.debitAmount}</b></li>
-      ))
-    }
-    </ul>
-  </div>
-);
+export const BillInfoComponent = (props: BillInfoComponentProps) => {
+  let total = 0;
+  for (let i = 0; i < props.transactions.length; i++) {
+    total += +props.transactions[i].debitAmount;
+  }
+
+  return (
+    <div>
+      <p>Next {props.type}:</p>
+      <ul>
+      {
+        props.transactions.map((transaction, idx) => (
+          <li key={idx}>{transaction.description}, taken around the {moment(transaction.date).format('Do')}, for about <b>&pound;{transaction.debitAmount}</b></li>
+        ))
+      }
+      </ul>
+      <p>
+        <b>Estimated total {props.type}</b>: &pound;{ total }
+      </p>
+    </div>
+  )
+}
